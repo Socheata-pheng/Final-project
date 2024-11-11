2575bf72a8fd10a4300740efd39cc7ec4cdbb041
Socheata-pheng <2023327pheng@aupp.edu.kh>
2024-11-11 14:58:52 +0700

    cheata

---
+++ home.js
@@ -0,0 +1,16 @@
+function updateTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    if (hours > 1 && hours <12){
+        document.getElementById('breakfast').style.display = 'block';
+    }
+    else if (hours >12 && hours < 19){
+        document.getElementById('lunch').style.display = 'block';
+    }
+    else if (hours > 19 && hours <24){
+        document.getElementById('dinner').style.display = 'block';
+    }
+}
+console.log(updateTime());
+
+
